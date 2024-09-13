f93861fbf7e90c6d82e759f7d04293f48644d5b3
Daniel Bento <dbento001@gmail.com>
2024-09-12 23:04:14 -0300

    Processo atualização estudos

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <TheHeader v-show="showHeader"></TheHeader>
+
+  <div v-show="showName">
+      Nome : {{ firstname }} <br>
+      Sobrenome : {{ lastname }}
+  </div>
+
+  <div v-if="accessLevel === 'admin'">Admin</div>
+  <div v-else-if="accessLevel === 'user'">User</div>
+  <div v-else>Marketing</div>
+
+  <img
+      alt="Vue logo"
+      src="./assets/logo.png"
+  >
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import TheHeader from './components/TheHeader'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    TheHeader
+  },
+  data() {
+    return {
+        showHeader: true,
+        firstname: 'Jon',
+        lastname: 'Snow',
+        showName: true,
+        accessLevel: 'marketing'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
