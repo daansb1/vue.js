9d66585f84d4e1acf9e22131ce7004c5b8d0330f
Daniel Bento <dbento001@gmail.com>
2024-09-12 23:22:33 -0300

    Atualizações

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,15 @@
   <div v-else-if="accessLevel === 'user'">User</div>
   <div v-else>Marketing</div>
 
+  <div>
+
+      <div v-for="(obj, index) in todos" v-bind:key="obj.title" class="todos-itens" >
+        {{index}} - {{obj.title}}
+      </div>
+
+  </div>
+
+
   <img
       alt="Vue logo"
       src="./assets/logo.png"
@@ -33,13 +42,59 @@ export default {
         firstname: 'Jon',
         lastname: 'Snow',
         showName: true,
-        accessLevel: 'marketing'
+        accessLevel: 'marketing',
+        todos: [
+          {
+            "albumId": 1,
+            "id": 1,
+            "title": "accusamus beatae ad facilis cum similique qui sunt",
+            "url": "https://via.placeholder.com/600/92c952",
+            "thumbnailUrl": "https://via.placeholder.com/150/92c952"
+          },
+          {
+            "albumId": 1,
+            "id": 2,
+            "title": "reprehenderit est deserunt velit ipsam",
+            "url": "https://via.placeholder.com/600/771796",
+            "thumbnailUrl": "https://via.placeholder.com/150/771796"
+          },
+          {
+            "albumId": 1,
+            "id": 3,
+            "title": "officia porro iure quia iusto qui ipsa ut modi",
+            "url": "https://via.placeholder.com/600/24f355",
+            "thumbnailUrl": "https://via.placeholder.com/150/24f355"
+          },
+          {
+            "albumId": 1,
+            "id": 4,
+            "title": "culpa odio esse rerum omnis laboriosam voluptate repudiandae",
+            "url": "https://via.placeholder.com/600/d32776",
+            "thumbnailUrl": "https://via.placeholder.com/150/d32776"
+          },
+          {
+            "albumId": 1,
+            "id": 5,
+            "title": "natus nisi omnis corporis facere molestiae rerum in",
+            "url": "https://via.placeholder.com/600/f66b97",
+            "thumbnailUrl": "https://via.placeholder.com/150/f66b97"
+          }
+        ]
     }
   }
 }
 </script>
 
 <style>
+.todos-itens{
+    background: crimson;
+    margin: 0 0 5px 0;
+    padding: 3px 6px;
+    color: white;
+
+}
+
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
